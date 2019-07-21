7b94c2960076d48ca8beb53c82e41405155889ed
Sawanon <sawanon01@hotmail.com>
2019-07-22 02:55:30 +0700

    update

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <title></title>
-  </head>
-  <body>
-    <div class="">
-      asdas
-    </div>
-  </body>
-  <script src="bootstrap/js/bootstrap.js"></script>
-</html>
+<?php
+include("header.php");
+ ?>
+
+<!-- Begin page content -->
+<main role="main" class="flex-shrink-0" style="padding-top: 60px;">
+  <div class="container">
+    <h1 class="mt-5">เว็บจำลองการซื้อต้นไม้ผ่านตู้ขายต้นไม้อัตโนมัติ</h1>
+    <p>Test</p>
+  </div>
+</main>
+
+<footer class="footer mt-auto py-3">
+  <div class="container">
+    <span class="text-muted">Place sticky footer content here.</span>
+  </div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"><\/script>')</script><script src="/docs/4.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
+
+</body></html>
