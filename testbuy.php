6006f530d3b8d3531f985f0f1fd8f05effd4b167
Sawanon <sawanon01@hotmail.com>
2019-07-22 12:02:52 +0700

    update

--- testbuy.php
+++ testbuy.php
@@ -5,8 +5,8 @@ include("header.php");
 <main role="main" class="flex-shrink-0" style="padding-top: 60px;">
   <div class="row">
     <div class="col-12">
-      <a href="http://192.168.1.2/on"><button class="btn btn-primary" type="button" name="button">ต้นที่ 1</button></a>
-      <a href="http://192.168.1.2/on"><button class="btn btn-primary" type="button" name="button">ต้นที่ 2</button></a>
+      <a href="http://192.168.1.2/on1"><button class="btn btn-primary" type="button" name="button">ต้นที่ 1</button></a>
+      <a href="http://192.168.1.2/on2"><button class="btn btn-primary" type="button" name="button">ต้นที่ 2</button></a>
     </div>
   </div>
 </main>
